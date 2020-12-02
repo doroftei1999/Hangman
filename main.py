c1845a5de9cd097a499f2ecdade8a00cb7e6ff8f
doroftei1999 <doroftei1999@yahoo.com>
2020-12-02 17:57:02 +0200

    Started the Ui

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLineEdit
 
 
-class Ui(QMainWindow)
+class Ui(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Spanzuratoarea")
+        self.setFixedSize(360, 640)
+
 
 def main():
     app = QApplication(sys.argv)
-
+    ui = Ui()
+    ui.show()
     sys.exit(app.exec())
 
 
