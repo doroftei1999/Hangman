5725a622a3b20ebdddc809de16ad40d05400a238
doroftei1999 <doroftei1999@yahoo.com>
2020-12-02 17:49:52 +0200

    Started the Ui

---
+++ main.py
@@ -0,0 +1,18 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow
+
+
+class Ui(QMainWindow)
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Spanzuratoarea")
+
+def main():
+    app = QApplication(sys.argv)
+
+    sys.exit(app.exec())
+
+
+if __name__ == '__main__':
+    main()
+
